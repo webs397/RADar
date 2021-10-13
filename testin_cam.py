d2cdac0b1782448aa9c653fbb13b18780d9faf8c
Olli Kui <oliver.kuipers@fh-aachen.de>
2021-10-13 18:16:20 +0200

    added a testing sequence for the raspberry pi to test

---
+++ testin_cam.py
@@ -0,0 +1,7 @@
+import sys
+from RADar.camera.camera import CamConfigHandler
+from camera import *
+
+
+config_fucker = CamConfigHandler()
+config_fucker.print_config()
---
+++ testing_cam.py
@@ -0,0 +1,6 @@
+import sys
+from camera.dashcam import *
+
+
+config_fucker = CamConfigHandler()
+config_fucker.print_config()
