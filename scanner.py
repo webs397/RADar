6e8b4ac8e58e901686a1ce172222c59a332d8cfd
webs397 <benevans07@googlemaiil.com>
2021-10-13 22:01:21 +0200

    Testing

--- scanner.py
+++ scanner.py
@@ -2,6 +2,8 @@
 #import ultrasonic
 import time
 import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
 LEDs = [37,35,33,31,29,40,38,36,32,22]
 GPIO.setup(LEDs[0], GPIO.OUT)  # this is pin 37
 GPIO.setup(LEDs[1], GPIO.OUT)  # this is pin 35
