94a8368d12769800a4b56e47f6793d7a119db691
Olli Kui <oliver.kuipers@fh-aachen.de>
2021-10-23 22:58:00 +0200

    added print statement for debugging purposes

--- esp_com/udp_server.py
+++ esp_com/udp_server.py
@@ -5,6 +5,7 @@ class Server:
     def __init__(self, client_ip_address, port):
         self.address = (client_ip_address,port)
         self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        print(self.address)
         self.udp_socket.bind(self.address)
 
     def receive_data(self):
