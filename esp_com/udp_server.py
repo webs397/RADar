e486fa654af3868254d7cfc72702c4fdf5d15709
Olli Kui <oliver.kuipers@fh-aachen.de>
2021-10-23 22:50:48 +0200

    first try to make the esp32 and the raspberry pi speak with each other. The raspi is the server, the esp the client.

---
+++ esp_com/udp_server.py
@@ -0,0 +1,24 @@
+import socket
+import json
+
+class Server:
+    def __init__(self, client_ip_address, port):
+        self.address = (client_ip_address,port)
+        self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.udp_socket.bind(self.address)
+
+    def receive_data(self):
+        print('waiting for messages ...')
+        while True:
+            try:
+                (data, addr) = self.udp_socket.recvfrom()
+                data = json.loads(data)
+                print('received: ', data, 'from :', addr)
+            except KeyboardInterrupt:
+                self.udp_socket.close()
+    
+    def close_connection(self):
+        self.udp_socket.close()
+
+myserver = Server('192.168.0.69', 50000)
+myserver.receive_data()
