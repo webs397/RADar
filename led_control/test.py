852c366174cfc7a9fb87f8256afcf84882dcf670
Olli Kui <oliver.kuipers@fh-aachen.de>
2021-11-06 14:37:26 +0100

    refined it again

--- led_control/test.py
+++ led_control/test.py
@@ -5,10 +5,9 @@ inp = None
 while True:
     try:
         print("distance: ")
-        inp = input()
-        dist= 5
+        inp = int(input())
         led_controller = LEDCONTROLLER(PIN_DATA, PIN_LATCH, PIN_CLOCK)
-        led_controller.led_array(dist)
+        led_controller.led_array(inp)
     except ValueError:
         print('input was not a number')
         break
