981513022842b0f1dda1d1a29c54d0fcb4821d09
Olli Kui <oliver.kuipers@fh-aachen.de>
2021-11-06 17:37:52 +0100

    altered the test

--- led_control/test.py
+++ led_control/test.py
@@ -5,11 +5,9 @@ led_controller = Shifter()
 
 while True:
     try:
-        #print("distance: ")
-        #inp = int(input())
-        for i in range(1,21):
-            led_controller.setLights(i)
-            time.sleep(1)
+        print("distance: ")
+        inp = int(input())
+        led_controller.setLights(inp)
     except Exception as e:
         print('There was an error:', e)
         break
