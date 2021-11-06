6d76e2e06f90454608ac9f87f54009ebf15201d3
Olli Kui <oliver.kuipers@fh-aachen.de>
2021-11-06 14:36:17 +0100

    refined the test for philipps code

--- led_control/test.py
+++ led_control/test.py
@@ -1,5 +1,14 @@
 from control import *
 
-dist= 5
-led_controller = LEDCONTROLLER(PIN_DATA, PIN_LATCH, PIN_CLOCK)
-led_controller.led_array(dist)
+inp = None
+
+while True:
+    try:
+        print("distance: ")
+        inp = input()
+        dist= 5
+        led_controller = LEDCONTROLLER(PIN_DATA, PIN_LATCH, PIN_CLOCK)
+        led_controller.led_array(dist)
+    except ValueError:
+        print('input was not a number')
+        break
