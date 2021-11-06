8126e3245f352afbfde7f667fe8a573a78e99c35
Olli Kui <oliver.kuipers@fh-aachen.de>
2021-11-06 17:26:23 +0100

    trying to find my mistake in tesiting

--- led_control/test.py
+++ led_control/test.py
@@ -1,13 +1,15 @@
 from newControl import *
-
+import time
 inp = None
 led_controller = Shifter
 
 while True:
     try:
-        print("distance: ")
-        inp = int(input())
-        led_controller.setLights(inp)
+        #print("distance: ")
+        #inp = int(input())
+        for i in range(1,21):
+            led_controller.setLights(i)
+            time.sleep(1)
     except Exception as e:
         print('There was an error:', e)
         break
