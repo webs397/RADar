bdfa3b7edeac606bdc0c26e5ad64821e280ccdbf
Olli Kui <oliver.kuipers@fh-aachen.de>
2021-11-08 01:13:58 +0100

    finished the button

--- raspicode_1.py
+++ raspicode_1.py
@@ -5,6 +5,8 @@ from esp_com.pi_udp_server import *
 from led_control.olliscontroller import *
 from wifi_helper import *
 import RPi.GPIO as GPIO
+from internet_finder import *
+import os
 
 
 # ---EXAMPLE PACKAGE---
@@ -83,6 +85,7 @@ if __name__ == '__main__':
                     GPIO.output(BLUE_LED, GPIO.LOW)
                     time.sleep(0.25)
                     break
+                time.sleep(1)
 
             if button_status == 0:
                 # IDLE
@@ -95,27 +98,32 @@ if __name__ == '__main__':
             elif button_status == 1:
                 # CONNECT & RECEIVE
                 print('status: CONNECT & REICEIVE')
-                if just_switched:
-                    GPIO.output(GREEN_LED, GPIO.HIGH)
-                    GPIO.output(BLUE_LED, GPIO.LOW)
-                    just_switched = False
-                if myalarm.compute_measurments():
-                    msg = {'reason':'danger'}
+                if get_connect_status:
+                    # try to find the esp network
+                    print('reconfiguring network')
+                    os.system("wpa_cli -i wlan0 reconfigure")
+                    time.sleep(3)
+                else:
+                    if just_switched:
+                        GPIO.output(GREEN_LED, GPIO.HIGH)
+                        GPIO.output(BLUE_LED, GPIO.LOW)
+                        just_switched = False
+                    if myalarm.compute_measurments():
+                        msg = {'reason':'danger'}
 
-                '''
-                if button pressed:
-                    msg = {'reason':'demand'}
-                '''
+                    '''
+                    if button pressed:
+                        msg = {'reason':'demand'}
+                    '''
+                    
+                    # Daten kriegen und die LEDs entsprechend schalten
+                    esp_msg = networker.server.receive_data()
+                    print(esp_msg)
+                    # --> philipps led-steuercode wird benoetigt
+                    #print(esp_msg)
+                    #if not(esp_msg == None):
+                        #led_controller.disp_leds(esp_msg)
                 
-                # Daten kriegen und die LEDs entsprechend schalten
-                esp_msg = networker.server.receive_data()
-                print(esp_msg)
-                # --> philipps led-steuercode wird benoetigt
-                #print(esp_msg)
-                #if not(esp_msg == None):
-                    #led_controller.disp_leds(esp_msg)
-                
-
             if button_status == 2:
                 # UPLOAD
                 print('status: UPLOAD')
@@ -123,12 +131,20 @@ if __name__ == '__main__':
                     GPIO.output(GREEN_LED, GPIO.LOW)
                     GPIO.output(BLUE_LED, GPIO.HIGH)
                     just_switched = False
-            
+
+                if get_connect_status:
+                    # connect to cloud an upload
+                    print('uploaded')
+                    button_status = 0
+                else:
+                    print('reconfiguring network')
+                    os.system("wpa_cli -i wlan0 reconfigure")
+                    time.sleep(3)
+
             # Frequenz setzen
             time.sleep(1/frequenz)
     except KeyboardInterrupt:
         print('aborting')
-        #GPIO.cleanup()
     except Exception as e:
         print('there was the following error: ', e)
     finally:
