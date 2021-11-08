51a6d5e5fdf8e96a554bf4f44d2a14fc5202c75a
Olli Kui <oliver.kuipers@fh-aachen.de>
2021-11-08 02:09:02 +0100

    finished the google thingy - hopefully

--- uploading/demo.py
+++ uploading/demo.py
@@ -1,3 +1,4 @@
+from googleapiclient.http import MediaFileUpload
 from Google import Create_Service
 
 CLIENT_SECRET_FILE = 'client_secret_file.json'
@@ -7,4 +8,20 @@ SCOPES = ['https://www.googleapis.com/auth/drive']
 
 service = Create_Service(CLIENT_SECRET_FILE,API_NAME,API_VERSION,SCOPES)
 
-print(dir(service))
+folder_id = '1pHL26t0oeDGo7h0FmMMQ08e76J0JaTRn'
+
+file_names = []
+mime_types = ['application/vnd.google-apps.video']
+
+for file_name in file_names:
+    file_metadata={
+        'name': file_name,
+        'parents': [folder_id]
+    }
+
+    media = MediaFileUpload('~/RADar/danger/{0}'.format(file_name), mimetype='application/vnd.google-apps.video')
+    service.files().create(
+        body=file.metadata,
+        media_body=media,
+        fields='id'
+    ).execute()
